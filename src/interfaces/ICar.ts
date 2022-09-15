0440a9dc34314d48f85cda99078879c9e904c551
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-15 15:23:33 -0300

    :sparkles: feat: create ICar interface

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { IVehicleZodSchema } from './IVehicle';
+
+export const ICarZodSchema = IVehicleZodSchema.extend({
+  doorsQty: z.number().int().gte(2).lte(4),
+  seatsQty: z.number().int().gte(2).lte(7),
+});
+
+export type ICar = z.infer<typeof ICarZodSchema>;
