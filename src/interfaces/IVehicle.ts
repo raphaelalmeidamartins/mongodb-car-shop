dd542749ec8b844dcd1caa69e0e34595389762df
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-15 15:23:21 -0300

    :recycle: refactor: import zod schema, too

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const IVehicleZodSchema = z.object({
+export const IVehicleZodSchema = z.object({
   model: z.string().min(3),
   year: z.number().int().gte(1900).lte(2022),
   color: z.string().min(3),
