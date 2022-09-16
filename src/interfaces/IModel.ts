129256eb852a7bab1c249a9510b5de1f00f086be
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-16 01:03:08 +0000

    :recycle: refactor: change param name

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,7 +1,7 @@
 export interface IModel<Entity> {
   create(data: Entity): Promise<Entity>;
   read(): Promise<Entity[]>;
-  readOne(pk: string): Promise<Entity | null>;
-  update(pk: string, data: Entity): Promise<Entity | null>;
-  delete(pk: string): Promise<Entity | null>;
+  readOne(_id: string): Promise<Entity | null>;
+  update(_id: string, data: Entity): Promise<Entity | null>;
+  delete(_id: string): Promise<Entity | null>;
 }
