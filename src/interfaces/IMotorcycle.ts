e80c035b0b442b7efaa6eb77706e88c81bdb787a
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-16 20:07:04 -0300

    :bug: fix: add gt 0

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -3,7 +3,7 @@ import { IVehicleZodSchema } from './IVehicle';
 
 export const IMotorcycleZodSchema = IVehicleZodSchema.extend({
   category: z.enum(['Street', 'Custom', 'Trail']),
-  engineCapacity: z.number().int().gte(2500),
+  engineCapacity: z.number().int().gt(0).lte(2500),
 });
 
 export type IMotorcycle = z.infer<typeof IMotorcycleZodSchema>;
