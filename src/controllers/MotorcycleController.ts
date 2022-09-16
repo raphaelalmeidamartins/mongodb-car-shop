21fbd0c4e7607caf4544113fb2c9f79adb413cdf
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-16 20:08:40 -0300

    :sparkles: feat: create motorcycle controller

---
+++ src/controllers/MotorcycleController.ts
@@ -0,0 +1,6 @@
+import { IMotorcycle } from '../interfaces/IMotorcycle';
+import Controller from './Controller';
+
+class MotorcycleController extends Controller<IMotorcycle> {}
+
+export default MotorcycleController;
