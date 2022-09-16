f38b409288691878313040caca510e7410f8d4a7
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-15 22:08:36 -0300

    :sparkles: feat: add car routes and error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
+import carsRoutes from './routes/cars';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/cars', carsRoutes);
+
+app.use(errorHandler);
 
 export default app;
