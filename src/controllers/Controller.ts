ed86d46ba7f8f2322e9f38d0448a43a18321d7be
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-15 22:07:18 -0300

    :sparkles: feat: create Controller abstract class

---
+++ src/controllers/Controller.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+import StatusCodes from '../helpers/StatusCodes';
+import { IController } from '../interfaces/IController';
+import Service from '../services/Service';
+
+abstract class Controller<Entity> implements IController {
+  constructor(protected _service: Service<Entity>) {
+    this.register = this.register.bind(this);
+  }
+
+  public async register(req: Request, res: Response, _next: NextFunction): Promise<void> {
+    const document = await this._service.create(req.body);
+
+    res.status(StatusCodes.Created).json(document);
+  }
+}
+
+export default Controller;
