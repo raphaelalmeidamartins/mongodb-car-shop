a4c0fe66b7de70761ea0e8f576f85106738eda26
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-16 01:04:04 +0000

    :sparkles: feat: create IService interface

---
+++ src/interfaces/IService.ts
@@ -0,0 +1,7 @@
+export interface IService<Entity> {
+  create(data: Entity): Promise<Entity>;
+  read(): Promise<Entity[]>;
+  readOne(_id: string): Promise<Entity | null>;
+  update(_id: string, data: Entity): Promise<Entity | null>;
+  delete(_id: string): Promise<Entity | null>;
+}
