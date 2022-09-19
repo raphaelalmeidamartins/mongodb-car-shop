65a07c18d833b23279a9e4c3c62f9d760c91d46d
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-09-19 13:24:04 -0300

    :books: docs: create swagger settings files

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
 import express from 'express';
 import 'express-async-errors';
+import swaggerUI from 'swagger-ui-express';
 import errorHandler from './middlewares/error';
 import carsRoutes from './routes/cars';
 import motorcyclesRoutes from './routes/motorcycles';
+import swaggerSettingsBr from './swagger-br.json';
+import swaggerSettingsEn from './swagger-en.json';
 
 const app = express();
+
+const options = {};
+
+app.use(
+  '/docs/br',
+  swaggerUI.serveFiles(swaggerSettingsBr, options),
+  swaggerUI.setup(swaggerSettingsBr),
+);
+app.use(
+  '/docs/en',
+  swaggerUI.serveFiles(swaggerSettingsEn, options),
+  swaggerUI.setup(swaggerSettingsEn),
+);
 
 app.use(express.json());
 
